08a7badadf98475ef5922c27fb4e700cbdb66c01
amine <amine@videri-concept.de>
2016-10-05 11:56:52 +0200

    nginx config file

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
@@ -65,7 +65,8 @@ namespace :deploy do
   task :initial do
     on roles(:app) do
       before 'deploy:restart', 'puma:start'
-      Rake::Task['puma:restart'].reenable
+      Rake::Task['puma:restart'].
+      Rake::Task['puma:start'].reenable
       invoke 'deploy'
     end
   end
