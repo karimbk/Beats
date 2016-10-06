ee492bb5eaff6edafff75c712d5973966769256b
amine <amine@videri-concept.de>
2016-10-06 15:42:14 +0200

    auto deploy Sucess

--- config/deploy.rb
+++ config/deploy.rb
@@ -72,7 +72,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+      execute "pid=$(cat #{fetch(:puma_pid)}) && kill -s SIGUSR2 $pid"
     end
   end
 
