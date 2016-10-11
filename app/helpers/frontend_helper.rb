4dd512a0f7048d53efa335e3e0cac4c343ef3e16
Amine Belkhiria <amine@videri-concept.de>
2016-10-11 17:24:00 +0200

    frontend opt

--- app/helpers/frontend_helper.rb
+++ app/helpers/frontend_helper.rb
@@ -3,4 +3,8 @@ module FrontendHelper
   def beats_menu_activ(c_name, second = true)
     return 'activ' if action_name == c_name && second
   end
+    
+  def set_black_footer
+    return 'black-text' if action_name == 'index'
+  end
 end
