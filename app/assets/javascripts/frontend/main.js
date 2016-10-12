06ceb5e7da589de6aeb505d9e664aeedf25a2287
Amine Belkhiria <amine@videri-concept.de>
2016-10-13 00:23:39 +0200

    mobile side Nav bar frontend

--- app/assets/javascripts/frontend/main.js
+++ app/assets/javascripts/frontend/main.js
@@ -2,3 +2,14 @@
 
 },{}]},{},[1])
 
+/* Mobile SideNav */
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    console.log("side nav open")
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
